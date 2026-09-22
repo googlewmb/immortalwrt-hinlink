7fd093a124eb4a0e48229a46860a8802028f42e8
googlewmb <58500194+googlewmb@users.noreply.github.com>
2026-09-23 02:24:44 +0800

    添加独立固件工程：按板级硬件分配默认驱动

--- overlay/target/linux/rockchip/image/h6xk.mk
+++ overlay/target/linux/rockchip/image/h6xk.mk
@@ -12,7 +12,7 @@ define Device/h6xk-common
   DEVICE_DTS_DIR := ../dts
   UBOOT_DEVICE_NAME := easepi-rk3568
   KERNEL := kernel-bin
-  DEVICE_PACKAGES := kmod-r8125 kmod-r8168 kmod-ata-ahci-dwc kmod-hwmon-pwmfan kmod-thermal kmod-iio-rockchip-saradc
+  DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci-dwc kmod-thermal kmod-iio-rockchip-saradc
   IMAGE/sysupgrade.img.gz = h6xk-boot | boot-script h6xk | pine64-img | gzip | append-metadata
 endef
 
@@ -38,6 +38,7 @@ TARGET_DEVICES += hinlink_opc-h68k
 define Device/hinlink_opc-h69k
   $(call Device/h6xk-common)
   DEVICE_MODEL := OPC-H69K
+  DEVICE_PACKAGES += kmod-hwmon-pwmfan kmod-usb-net-qmi-wwan kmod-usb-net-cdc-mbim kmod-usb-serial-option
   DEVICE_DTS := rk3568/rk3568-opc-h69k
   SUPPORTED_DEVICES := hinlink,opc-h69k
 endef
